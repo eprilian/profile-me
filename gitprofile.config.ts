72a208c373ff830eaa25088d43910ae30e69e083
noname.xyz <57064161+eprilian@users.noreply.github.com>
2025-11-24 06:51:13 +0700

    Change base path in gitprofile configuration

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -10,7 +10,7 @@ const CONFIG = {
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
   /** base: '/gitprofile/', */
-  base: '/profile-me/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
